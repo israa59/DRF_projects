330c5b0bcc3f6186be63fa95ee4e34a125d4852c
Israa Irshad <israa@qburst.com>
2023-10-17 17:48:39 +0530

    Added a user profile model

--- drf_permissions/create_user_profile/models.py
+++ drf_permissions/create_user_profile/models.py
@@ -1,2 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
+    bio = models.TextField(blank=True)
+    display_picture = models.ImageField()
